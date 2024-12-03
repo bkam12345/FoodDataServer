5c9eca2dbea2262bebe9440067a9f2d2eee1bcfe
周恭煥 <kenchou2006@gmail.com>
2024-12-03 14:33:09 +0800

    Fix wsgi

--- server/settings.py
+++ server/settings.py
@@ -124,9 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_SUPERUSER_USERNAME = os.getenv('DEFAULT_SUPERUSER_USERNAME', 'admin')
+DEFAULT_SUPERUSER_EMAIL = os.getenv('DEFAULT_SUPERUSER_EMAIL', 'admin@example.com')
+DEFAULT_SUPERUSER_PASSWORD = os.getenv('DEFAULT_SUPERUSER_PASSWORD', 'password')
